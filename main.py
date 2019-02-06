4d0c9c192c2d01200dd358629c225c3dfa2b0fa9
zhan0903 <z.han0903@qq.com>
2019-02-06 22:07:39 +1100

    test

--- main.py
+++ main.py
@@ -255,7 +255,7 @@ class Agent:
             state = next_state
         if store_transition: self.num_games += 1
         replay_memory.put(total_reward)
-        print(total_reward)
+        # print(total_reward)
         print(len(self.replay_buffer))
         # return total_reward
 
