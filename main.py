036c6cf9ef4bc6cd3af0221e2c032766d59ee339
zhan0903 <z.han0903@qq.com>
2019-02-13 10:42:06 +1100

    test

--- main.py
+++ main.py
@@ -231,7 +231,7 @@ class Agent:
         print("self.num_frames ", self.num_frames)
         print("steps", self.learner.steps)
 
-        print(self.replay_memory[:2])
+        print(len(self.replay_memory))
 
 
         # for i in range(self.args.pop_size):
